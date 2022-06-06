497d3d4bd4e999434d693f194926d9a856b46d54
xwmyth8023 <xwmyth8023@163.com>
2022-06-06 23:05:36 +0800

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push('--disable-dev-shm-usage')
+          launchOptions.args.push('--disable-gpu')
+        }
+        return launchOptions
+      })
       // implement node event listeners here
     },
   },
