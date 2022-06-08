1279ab38de289fc8476441990110b610d679d578
xwmyth8023 <xwmyth8023@163.com>
2022-06-08 22:16:24 +0800

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,15 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
-});
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push('--disable-dev-shm-usage')
+          launchOptions.args.push('--disable-gpu')
+          launchOptions.args.push('--disable-extensions')
+          launchOptions.args.push('--ignore-certificate-errors')
+        }
+        return launchOptions
+      })
+    }
+  }
+})
