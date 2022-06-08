b8055aa4d31e07c5879f7fd8096b5ff96adccac6
xwmyth8023 <xwmyth8023@163.com>
2022-06-09 00:03:46 +0800

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
+    videosFolder: this.video ? "cypress/videos" : '',
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('before:browser:launch', (browser = {}, launchOptions) => {
@@ -10,6 +13,7 @@ module.exports = defineConfig({
           launchOptions.args.push('--disable-gpu')
           launchOptions.args.push('--disable-extensions')
           launchOptions.args.push('--ignore-certificate-errors')
+          launchOptions.args.push('--disable-software-rasterizer')
         }
         return launchOptions
       })
